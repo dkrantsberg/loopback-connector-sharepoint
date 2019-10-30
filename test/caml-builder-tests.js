43cbf9ae1d971aea2f32a16e3d4eb79cbe8d0427
dkrantsberg <dkrants@gmail.com>
2019-10-30 16:23:03 -0400

    fixed `inq` condition

--- test/caml-builder-tests.js
+++ test/caml-builder-tests.js
@@ -44,6 +44,12 @@ describe('CamlBuilder', () => {
       const expectedResult = `<Where><Eq><FieldRef Name="LastName"/><Value Type="Text">Doe</Value></Eq></Where>`;
       expect(result).to.eql(expectedResult);
     });
+    it.only(`'inq' condition`, () => {
+      const where = {ID: {inq: [4, 5, 6, 7]}};
+      const result = camlBuilder.buildWhere(where);
+      const expectedResult = `<Where><In><FieldRef Name="ID"/><Values><Value Type="Number">4</Value><Value Type="Number">5</Value><Value Type="Number">6</Value><Value Type="Number">7</Value></Values></In></Where>`;
+      expect(result).to.eql(expectedResult);
+    });
     it('2 conditions with logical AND', () => {
       const where = {and: [{firstName: 'Joe'}, {lastName: 'Doe'}]};
       const expectedResult = `<Where><And><Eq><FieldRef Name="FirstName"/><Value Type="Text">Joe</Value></Eq><Eq><FieldRef Name="LastName"/><Value Type="Text">Doe</Value></Eq></And></Where>`;
@@ -62,7 +68,7 @@ describe('CamlBuilder', () => {
       const result = camlBuilder.buildWhere(where);
       expect(result).to.eql(expectedResult);
     });
-    it.only('combination of AND and OR conditions', () => {
+    it('combination of AND and OR conditions', () => {
       const where = {and: [{or: [{firstName: 'Joe'}, {lastName: 'Doe'}]}, {email: 'joe.doe@company.com'}]};
       const expectedResult = `<Where><And><Or><Eq><FieldRef Name="FirstName"/><Value Type="Text">Joe</Value></Eq><Eq><FieldRef Name="LastName"/><Value Type="Text">Doe</Value></Eq></Or><Eq><FieldRef Name="Email"/><Value Type="Text">joe.doe@company.com</Value></Eq></And></Where>`;
       const result = camlBuilder.buildWhere(where);
